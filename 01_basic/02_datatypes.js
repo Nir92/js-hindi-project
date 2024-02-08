37360a453f205f3241459278868e3e847a65b427
Nir92 <98165922+Nir92@users.noreply.github.com>
2024-02-08 08:30:22 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,9 @@
+"use strict";
+
+console.log(3+3)
+console.log(typeof "nir");//string\\
+console.log(typeof age);//number\\
+console.log(typeof null);//object\\
+console.log(typeof underdefine);//underdefined\\
+console.log(typeof age);
+
